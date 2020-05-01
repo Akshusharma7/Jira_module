a1d73bb78c3127b186780b729facef84ec1d3fbe
Prateek Sharma <44917774+Akshusharma7@users.noreply.github.com>
2020-05-01 10:58:23 +0530

    Code to create Ticket in Jira through Python Code

---
+++ Create_ticket_jira.py
@@ -0,0 +1,28 @@
+import datetime
+from jira import JIRA
+
+
+def jira_setup(project, assignee, title, description, filename='', due=1, comment=''):
+    apiKey = 'QnucOPUGJMdyos3Yg3ZvE83C'
+    jira = JIRA(basic_auth=('will.hickson@shoregrp.com', apiKey),
+                options={"server": 'https://shoregroup.atlassian.net'})
+    duedate = str((datetime.date.today() + datetime.timedelta(days=due)).strftime('%Y-%m-%d'))
+    issue_list = [{
+        'project': {'key': project},
+        'summary': title,
+        'description': description,
+        'issuetype': {'name': 'Task'},
+        'assignee': {'name': assignee},
+        'priority': {'name': 'High'},
+        'duedate': duedate
+    }]
+    issues = jira.create_issues(issue_list)
+    issueKey = issues[0]['issue'].key
+    print(f'{issueKey} created')
+    if filename:
+        with open(filename, 'rb') as f:
+            jira.add_attachment(issue=issueKey, attachment=f)
+    if comment:
+        jira.add_comment(issue=issueKey, body=comment)
+        print(f'Comment added to {issueKey}')
+    return issueKey
