1ab5e22d8adb52e0be749a8955318f5583836445
Prateek Sharma <44917774+Akshusharma7@users.noreply.github.com>
2020-05-01 11:03:02 +0530

    Update Create_ticket_jira.py

--- Create_ticket_jira.py
+++ Create_ticket_jira.py
@@ -5,7 +5,7 @@ from jira import JIRA
 def jira_setup(project, assignee, title, description, filename='', due=1, comment=''):
     '''This code will Create Jira ticket and return the Issue Key of the ticket Created. '''
     
-    apiKey = '*********************'
+    apiKey = '*********************' #apiKey you will find in Jira page
     jira = JIRA(basic_auth=('abcdef@gmail.com', apiKey),
                 options={"server": 'https://abcdef.xyz.net'})
     duedate = str((datetime.date.today() + datetime.timedelta(days=due)).strftime('%Y-%m-%d'))
